86d0c4d880eeeca3e6a4e20e09c06016f020dc25
ryan schulman <schulmanworks@gmail.com>
2017-09-13 16:23:08 -0600

    opened up test file to all additions

--- test_syscall.c
+++ test_syscall.c
@@ -6,8 +6,8 @@
 #define __NR_print_self 338
 #define __NR_print_other 339
 int main(int argc, char *argv[]) {
-//    syscall(__NR_helloworld);
-    //syscall(__NR_print_self);
+    syscall(__NR_helloworld);
+    syscall(__NR_print_self);
     syscall(__NR_print_other);
     //syscall(400);
     return 0;
